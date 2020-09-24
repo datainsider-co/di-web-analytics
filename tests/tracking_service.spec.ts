4d3d7fff5b77090d9f5d4fe847d3b2965fd40df4
tvc12 - Thien Vi Chi <meomeocf98@gmail.com>
2020-09-24 17:40:58 +0700

    update: test case

--- tests/tracking_service.spec.ts
+++ tests/tracking_service.spec.ts
@@ -1,11 +1,33 @@
 import {trackingService} from '../src/service';
 import {expect} from 'chai';
+import {Property} from '../src/domain';
 
 describe('Test tracking api key', () => {
   const trackId = 'trace_id_test';
 
   const apiKey = 'test_api_key';
   const userId = 'test_user';
+
+  const defaultValues = {
+    diPlatform: 'web',
+    di_lib_version: '0.0.1',
+    di_tracking_id: trackId,
+    di_user_id: userId,
+    di_start_time: 0,
+    di_duration: 0,
+    di_time: Date.now()
+  } as Property;
+
+  const defaultEngageValues = {
+    diPlatform: 'web',
+    di_lib_version: '0.0.1',
+    di_tracking_id: trackId,
+    di_user_id: userId,
+    di_start_time: 0,
+    di_duration: 0,
+    di_time: Date.now(),
+    birth_date: Date.now(),
+  } as Property;
 
   it('Should generate api key success', async () => {
     const trackId = await trackingService.genTrackId(apiKey);
@@ -22,7 +44,8 @@ describe('Test tracking api key', () => {
       color: 'Red',
       connectivity: ['3G', '4G', '5G', 'Ethernet'],
       diUserId: userId,
-      createdAt: Date.now().toString()
+      createdAt: Date.now(),
+      ...defaultValues
     });
     console.log('track::trackId::', trackId);
     expect(trackId).not.undefined;
@@ -35,7 +58,8 @@ describe('Test tracking api key', () => {
       languageCode: 'vi',
       countryCode: 'vn',
       location: 'HCM city',
-      updatedAt: Date.now().toString()
+      updatedAt: Date.now(),
+      ...defaultEngageValues
     });
     console.log('track::trackId::', trackId);
     expect(trackId).not.undefined;
