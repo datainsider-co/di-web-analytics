1b32aaea4b78c7d5d3dd9affcf798a4cc1bccb8c
Thien Vi <meomeocf98@gmail.com>
2022-12-06 10:22:38 +0700

    update: test-case

--- karma.conf.js
+++ karma.conf.js
@@ -7,11 +7,14 @@ module.exports = function (config) {
       'tests/base_test_function.js',
       {pattern: 'tests/**/*.spec.js', included: true},
     ],
+    exclude: [
+      "tests/generate_data.spec.js"
+    ],
     reporters: ['progress'],
     port: 9876,  // karma web server port
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: [ 'Chrome', 'ChromeHeadless'],
+    browsers: [ 'Chrome', 'ChromeHeadless', 'Firefox'],
     autoWatch: false,
     concurrency: Infinity,
     customLaunchers: {
